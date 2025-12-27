41f39aee512986078ba854216adf08d4d26659ed
KaioAlvesDEV <157232955+KaioAlvesDEV@users.noreply.github.com>
2025-12-27 17:22:15 -0300

    Adiciona script para listar números pares e ímpares em Python
    Ele adiciona um novo arquivo Python chamado "UmaListaParesImpares.py" que contém um script para gerar e exibir uma lista de números pares e ímpares. Usando lista composta

---
+++ Python/UmaListaParesImpares.py
@@ -0,0 +1,26 @@
+# Criando uma lista com duas sublistas: uma para pares e outra para ímpares
+pares_impares = [[], []]
+
+# Coletando 7 números do usuário
+for i in range(1, 8):
+    
+    num = int(input(f'Digite o {i}º valor: '))
+
+    # Classificando números em pares e ímpares
+    if num % 2 == 0:
+        pares_impares[0].append(num)
+    else:
+        pares_impares[1].append(num)
+
+# Exibindo os números pares e ímpares
+print('Números pares digitados: ', end=': ')
+for par in pares_impares[0]:
+    print(par, end='')
+    if par != pares_impares[0][-1]:
+        print(',', end=' ')
+
+print('\nNúmeros ímpares digitados: ', end=': ')
+for impar in pares_impares[1]:
+    print(impar, end='')
+    if impar != pares_impares[1][-1]:
+        print(',', end=' ')
